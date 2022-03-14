f24b3bc2817e86198acb67febe0468fae7e1090b
mikelove <michaelisaiahlove@gmail.com>
2022-03-14 17:38:41 -0400

    first

---
+++ R/readEDS.R
@@ -0,0 +1,15 @@
+#' readEDS - a utility function for quickly reading in Alevin's EDS format
+#'
+#' @param numOfGenes number of genes
+#' @param numOfOriginalCells number of cells
+#' @param countMatFilename pointer to the EDS file, \code{quants_mat.gz}
+#' @param tierImport whether the \code{countMatFilename} refers to a quants tier file
+#'
+#' @return a genes x cells sparse matrix, of the class \code{dgCMatrix}
+#'
+#' @useDynLib eds
+#' 
+#' @export
+readEDS <- function(numOfGenes, numOfOriginalCells, countMatFilename, tierImport=FALSE) {
+  getSparseMatrix(numOfGenes, numOfOriginalCells, path.expand(countMatFilename), tierImport)
+}
